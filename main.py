e0b750110aaf5682ce5ce09702365df023d5f6fd
Saranyaraj Rajendran <saranyaraj4321@gmail.com>
2015-05-13 19:28:12 +0530

    Extracting url

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 
-page = '''<div id="top_bin"> <div id="top_content" class="width960">
-   <div class="udacity float-left"> <a href="/">'''
-
+page =('<div id="top_bin"><div id="top_content" class="width960">'
+'<div class="udacity float-left"><a href="http://udacity.com">')
 start_link = page.find('<a href="')
+start_link = start_link + len('<a href="')
+end_link = page.find('">', start_link)
+url = page[start_link:end_link]
+
+print url
