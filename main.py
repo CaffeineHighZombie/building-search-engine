c75a0e53c3826192c3393c2a8bad614b88c8114e
Saranyaraj Rajendran <saranyaraj4321@gmail.com>
2015-05-14 17:29:55 +0530

    Defining procedure for finding url

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 
 page =('<div id="top_bin"><div id="top_content" class="width960">'
 '<div class="udacity float-left"><a href="http://udacity.com">')
-start_link = page.find('<a href="')
-start_quote = page.find('"', start_link)
-end_quote = page.find('"', start_quote+1)
-url = page[start_quote+1:end_quote]
+
+def get_next_target(page):
+    start_link = page.find('<a href="')
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote+1)
+    url = page[start_quote+1:end_quote]
+    return url, end_quote
+
+end_quote = 0
+url, end_quote = get_next_target(page[end_quote:])
 
 print url
