2421a5e325bdc9c8f772e3fe6358d7d8256ecac8
Saranyaraj Rajendran <saranyaraj4321@gmail.com>
2015-05-18 19:45:16 +0530

    Added procedure to find and print all the links in a given page

--- main.py
+++ main.py
@@ -2,14 +2,24 @@
 page =('<div id="top_bin"><div id="top_content" class="width960">'
 '<div class="udacity float-left"><a href="http://udacity.com">')
 
+
 def get_next_target(page):
     start_link = page.find('<a href="')
-    start_quote = page.find('"', start_link)
-    end_quote = page.find('"', start_quote+1)
-    url = page[start_quote+1:end_quote]
-    return url, end_quote
 
-end_quote = 0
-url, end_quote = get_next_target(page[end_quote:])
+    if start_link == -1:
+        return None, 0
+    else:
+        start_quote = page.find('"', start_link)
+        end_quote = page.find('"', start_quote+1)
+        url = page[start_quote+1:end_quote]
+        return url, end_quote
 
-print url
+
+def print_all_links(page):
+    url, end_pos = get_next_target(page)
+    if url:
+        print url
+        print_all_links(page[end_pos:])
+
+
+print_all_links(page)
